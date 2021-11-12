acb334b2a7b9ffee328ec1edb9b8fc96e5b9e240
Maria Lipina <m.o.lip714@gmail.com>
2021-11-12 23:19:44 +0300

    revove // from code

--- Program.cs
+++ Program.cs
@@ -3,91 +3,91 @@ using static System.Convert;
 using System;
 
 // // Почувствуй себя интерном (семинар 04.11.2021)
-// WriteLine("0. Вывести квадрат числа");
+WriteLine("0. Вывести квадрат числа");
 
-// int x = ToInt32(ReadLine());
-// int power = x*x;
-// WriteLine(power);
+int x = ToInt32(ReadLine());
+int power = x*x;
+WriteLine(power);
 
-// WriteLine();
-// WriteLine("1. По двум заданным числам проверять является ли первое квадратом второго");
+WriteLine();
+WriteLine("1. По двум заданным числам проверять является ли первое квадратом второго");
 
-// bool IsQuadrat (int x, int y)
-// {
-// int z = x*y;
-// if (z==x) return true;
-// else return false; 
-// }
+bool IsQuadrat (int x, int y)
+{
+int z = x*y;
+if (z==x) return true;
+else return false; 
+}
 
-// int x1 = ToInt32(ReadLine());
-// int x2 = ToInt32(ReadLine());
-// WriteLine(IsQuadrat(x1, x2));
+int x1 = ToInt32(ReadLine());
+int x2 = ToInt32(ReadLine());
+WriteLine(IsQuadrat(x1, x2));
 
-// WriteLine();
-// WriteLine("2. Даны два числа. Показать большее и меньшее число");
+WriteLine();
+WriteLine("2. Даны два числа. Показать большее и меньшее число");
 
-// void Compare (int x, int y)
-// {
-// if (y>x) Console.WriteLine(y + " больше " + x);
-// else Console.WriteLine(x +" больше "+ y);
-// }
+void Compare (int x, int y)
+{
+if (y>x) Console.WriteLine(y + " больше " + x);
+else Console.WriteLine(x +" больше "+ y);
+}
 
-// int x3 = ToInt32(ReadLine());
-// int x4 = ToInt32(ReadLine());
-// Compare(x3, x4);
+int x3 = ToInt32(ReadLine());
+int x4 = ToInt32(ReadLine());
+Compare(x3, x4);
 
-// WriteLine();
-// WriteLine("3. По заданному номеру дня недели вывести его название");
+WriteLine();
+WriteLine("3. По заданному номеру дня недели вывести его название");
 
-// int day = ToInt32(ReadLine());
+int day = ToInt32(ReadLine());
 
-// string name = day switch
-// {
-// 1 => "понедельник",
-// 2 => "вторник",
-// 3 => "среда",
-// 4 => "четверг",
-// 5 => "пятница",
-// 6 => "суббота",
-// 7 => "воскресенье",
-// _ => "нет дня недели с таким номером"
-// };
-// WriteLine(name);
+string name = day switch
+{
+1 => "понедельник",
+2 => "вторник",
+3 => "среда",
+4 => "четверг",
+5 => "пятница",
+6 => "суббота",
+7 => "воскресенье",
+_ => "нет дня недели с таким номером"
+};
+WriteLine(name);
 
-// WriteLine();
-// WriteLine("4. Найти максимальное из 3 чисел");
+WriteLine();
+WriteLine("4. Найти максимальное из 3 чисел");
 
-// int num = ToInt32(ReadLine());
-// int num1 = ToInt32(ReadLine());
-// int num2 = ToInt32(ReadLine());
+int num = ToInt32(ReadLine());
+int num1 = ToInt32(ReadLine());
+int num2 = ToInt32(ReadLine());
 
-// int max = num1;
-// if (num1 > max) max = num1;
-// if (num2 > max) max = num2;
-// Console.WriteLine(max);
+int max = num1;
+if (num1 > max) max = num1;
+if (num2 > max) max = num2;
+Console.WriteLine(max);
 
-// WriteLine();
-// WriteLine("5. Написать программу вычисления значения функции y=f(a). Как уточнили потом: функция y=sin(a)");
+WriteLine();
+WriteLine("5. Написать программу вычисления значения функции y=f(a). Как уточнили потом: функция y=sin(a)");
 
-// double sin(double a)
-// {
-//     return Math.Asin(a); //тот самый момент когда код работает, но как? Непонятно
-// }
+double sin(double a)
+{
+    return Math.Asin(a); //тот самый момент когда код работает, но как? Непонятно
+}
 
-// double a = sin(ToDouble(ReadLine()));
-// Console.WriteLine(a);
+double a = sin(ToDouble(ReadLine()));
+Console.WriteLine(a);
 
-// WriteLine();
-// WriteLine("6. Выяснить является ли число чётным");
+WriteLine();
+WriteLine("6. Выяснить является ли число чётным");
 
-// int b = ToInt32(ReadLine());
+int b = ToInt32(ReadLine());
 
-// if (b%2==0){Console.WriteLine("четное");}
-// else {Console.WriteLine("нечетное");}
+if (b%2==0){Console.WriteLine("четное");}
+else {Console.WriteLine("нечетное");}
 
 
-// WriteLine();
-// WriteLine("7. Показать числа от -N до N");
+WriteLine();
+WriteLine("7. Показать числа от -N до N");
 
 void PrintArray(int[] collect)
 {
@@ -96,21 +96,21 @@ void PrintArray(int[] collect)
     WriteLine();
 }
 
-// int [] FillArray(int N)
-// {
-// int[] collect = new int[N+N+1];
-// for (int pos = 0; pos < collect.Length; pos++) 
-// {
-//     collect[pos] = -N;
-// N--; // тот самый момент, когда сначала написал код и только потом понял, почему он работает как надо.
-// }
-// return collect;
-// }
-// PrintArray(FillArray(ToInt32(ReadLine()))); // и превзошел самого себя по количеству скобочек на строку
+int [] FillArray(int N)
+{
+int[] collect = new int[N+N+1];
+for (int pos = 0; pos < collect.Length; pos++) 
+{
+    collect[pos] = -N;
+N--; // тот самый момент, когда сначала написал код и только потом понял, почему он работает как надо.
+}
+return collect;
+}
+PrintArray(FillArray(ToInt32(ReadLine()))); // и превзошел самого себя по количеству скобочек на строку
 
 
-// WriteLine();
-// WriteLine("8. Показать четные числа от 1 до N");
+WriteLine();
+WriteLine("8. Показать четные числа от 1 до N");
 
 int[] SelectEvens(int n)
 {
