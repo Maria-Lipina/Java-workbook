40980391c36ee12f5ac3f6420270a34b87c9cd63
Maria Lipina <m.o.lip714@gmail.com>
2021-11-15 23:22:26 +0300

    tasks 31-33 draft

--- Program.cs
+++ Program.cs
@@ -273,6 +273,7 @@ WriteLine("19. Определить номер четверти плоскост
 WriteLine();
 WriteLine("20. Задать номер четверти, показать диапазоны для возможных координат");
 WriteLine();
+
 WriteLine("21. Программа проверяет пятизначное число на палиндромом.");
 WriteLine();
 
@@ -495,9 +496,9 @@ string PrintMyArray(int[] collect)
     return outputString;
 }
 
-// int[] arr = CreateMyArray(8);
-// FillArray(arr, 10, 100);
-// string res = PrintMyArray(arr); WriteLine(res);
+int[] arr = CreateMyArray(8);
+FillArray(arr, 10, 100);
+string res = PrintMyArray(arr); WriteLine(res);
 
 //---------------------------------------------------------------------
 // Решение До. Сергей: в принципе это тоже работает, но можно лучше
@@ -516,15 +517,68 @@ string PrintMyArray(int[] collect)
 // }
 //-------------------------------------------------------------------
 
-// WriteLine();
-// WriteLine("32. Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран ");
+WriteLine();
+WriteLine("32. Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран ");
 
-// int[] arr1 = CreateMyArray(8);
-// FillArray(arr, 0, 1);
-// WriteLine(PrintMyArray(arr1));
+int[] arr1 = CreateMyArray(8);
+FillArray(arr, 0, 1);
+WriteLine(PrintMyArray(arr1));
 
-// WriteLine();
-// WriteLine("33. Задать массив из 12 элементов, заполненных числами из [0,9]. Найти сумму положительных/отрицательных элементов массива");
+WriteLine();
+WriteLine("33. Задать массив из 12 элементов, заполненных числами из [-9,9]. Найти сумму положительных/отрицательных элементов массива");
+
+int[] arr2 = CreateMyArray(12);
+FillArray(arr, -9, 9);
+WriteLine(PrintMyArray(arr2));
+
+int[] SelectBySign (int[] array, int sign)
+{
+//int [] selection = sign switch
+int [] result = new int [array.Length];
+int len = 0;
+switch (sign)
+{
+case < 0:
+for (int i = 0; i<= result.Length; i++)
+{
+    if (array[i] < 0)
+    {
+    result [len] = array[i];
+    len++;
+    }
+}
+Array.Resize(ref result, len);
+return result;
+break;
+
+case > 0:
+    for (int i = 0; i<= result.Length; i++)
+    {
+    if (array[i] > 0)
+    {
+    result [len] = array[i];
+    len++;
+    }
+    }
+Array.Resize(ref result, len);
+return result;
+break;
+
+case == 0:
+return array;
+break;
+};
+}
+
+int [] signed = SelectBySign(arr2, -1);
+WriteLine(PrintMyArray(signed));
+int [] unsigned = SelectBySign(arr2, +1);
+WriteLine(PrintMyArray(unsigned));
+
+int SignedSum = ArraySum(signed);
+WriteLine(SignedSum);
+int UnsignedSum = ArraySum(unsigned);
+WriteLine(UnsignedSum);
 
 
 // WriteLine();
