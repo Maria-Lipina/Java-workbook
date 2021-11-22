1089a633dba8802effae1eecba0e5c2352489a8a
Maria Lipina <m.o.lip714@gmail.com>
2021-11-22 23:29:23 +0300

    task 40

--- Program.cs
+++ Program.cs
@@ -788,8 +788,65 @@ return result;
 int [] B9 = PairsProd(A9);
 WriteLine(PrintMyArray(B9));
 
-// WriteLine();
-// WriteLine("40. В Указанном массиве вещественных чисел найдите разницу между максимальным и минимальным элементом");
+
+WriteLine();
+WriteLine("40. В Указанном массиве вещественных чисел найдите разницу между максимальным и минимальным элементом");
+
+double[] CreateArrayD(int length)
+{
+    return new double [length];
+}
+
+string PrintArrayD(double [] collect)
+{
+    string outputString = String.Empty;
+    for (int pos = 0; pos < collect.Length; pos++) outputString += $"{collect[pos]} | ";
+    return outputString;
+}
+
+void FillArrayD(double[] collection) // А тут войд можно и нужно, потому что метод только заполняет
+{
+    for (int i = 0; i < collection.Length; i++)
+    {
+        collection[i] = Math.Round((new Random().NextDouble()*100), 2);
+    }
+}
+
+
+double Findit (double [] array, string crat) //crat(erion) = 1 => find max; = 0 => find min;
+{
+    if (crat == "min")
+    {
+    double min = array[0];
+        int i = 1;
+        for (; i < array.Length; i++)
+        {
+            if (array[i] < min) min = array [i];
+        }
+    return min;
+    }
+    if (crat == "max")
+    {
+    double max = array[0];
+        int i = 1;
+        for (; i < array.Length; i++)
+        {
+            if (array[i] > max) max = array [i];
+        }
+    return max;
+    }
+    else return -1;
+}
+
+double [] arr3 = CreateArrayD(20);
+FillArrayD(arr3);
+WriteLine(PrintArrayD(arr3));
+
+double mini = Findit(arr3, "min");
+double maxi = Findit(arr3, "max");
+WriteLine($"min = {mini} / max = {maxi}");
+double diff = maxi - mini;
+WriteLine($"difference = {diff}");
 
 
 WriteLine("47. Написать программу копирования массива");
