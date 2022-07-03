93d55c45fc8c4dbfbe697bd4612ce762daf10bf9
Maria Lipina <m.o.lip714@gmail.com>
2022-07-03 12:13:32 +0300

    1. файл переименован в ArrayPermutation + изменено форматирование 2. ProdAndSum - изменено форматироване 3. MathPower добавлен 4. Полные тексты задач добавлены в readme

--- ProdAndSum.java
+++ ProdAndSum.java
@@ -1,15 +1,13 @@
 public class ProdAndSum {
-   
-    /* https://leetcode.com/problems/subtract-the-product-and-sum-of-digits-of-an-integer/ - мое решение*/
 
     /**
      * @param n
-     * @return разница между произведением и суммой цифр в числе n
+     * @return разница между произведением и суммой цифр в числе n.
      */
-    public static int subtractProductAndSum (int n) { 
-        int sum = 0; 
+    public static int subtractProductAndSum(int n) {
+        int sum = 0;
         int prod = 1;
-        while (n !=0) {
+        while (n != 0) {
             sum = sum + n % 10;
             prod = prod * (n % 10);
             n /= 10;
