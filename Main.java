b08bb65a7991955372952d6538d48c697f9b8bdb
Maria Lipina <m.o.lip714@gmail.com>
2022-07-06 17:55:11 +0300

    не всегда успешное разрешение конфликтов rebase

--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.FileWriter;
+import java.io.IOException;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        int[] arr = new int [6];
+        int k = 4;
+
+        FileWriter fw = new FileWriter("test.txt", true);
+        fw.append(NumsPermutation.generate(arr, k, 0));
     }
 }
