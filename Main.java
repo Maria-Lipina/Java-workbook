e32633497bd44c2ad9fec320456679c3e5a9a9c7
Maria Lipina <m.o.lip714@gmail.com>
2022-07-06 18:42:24 +0300

    убрала лишнее с ветки main

--- Main.java
+++ Main.java
@@ -1,12 +1,7 @@
-import java.io.FileWriter;
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        int[] arr = new int [6];
-        int k = 4;
 
-        FileWriter fw = new FileWriter("test.txt", true);
-        fw.append(NumsPermutation.generate(arr, k, 0));
     }
 }
