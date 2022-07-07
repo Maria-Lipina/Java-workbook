f0d5a05344d4166bf992d0836a6c9d229a5e8820
Maria Lipina <m.o.lip714@gmail.com>
2022-07-07 22:17:29 +0300

    с вызовом и полным текстом задачи в readme: RLE (compression), StringPalindrome

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-       
+    
+    System.out.println(RLE.compress("AAAADDDFRRRGHYYUUJ"));
+    System.out.println(StringPalindrome.isIt("негнипапинген"));
+
     }
 }
---
+++ RLE.java
@@ -0,0 +1,21 @@
+public class RLE {
+    public static String compress(String data) {
+        StringBuilder sb = new StringBuilder();
+        int counter = 1;
+        char t = data.charAt(0);
+        for (int i = 0; i < data.length(); i++) {
+            if (t != data.charAt(i)) {
+                sb.append(t);
+                if (counter != 1) {
+                    sb.append(counter);
+                }
+                t = data.charAt(i);
+                counter = 0;
+            }
+            counter++;
+        }
+        sb.append(String.format("%s%d", t, counter));
+        return sb.toString();
+    }
+}
+/*TODO: для лучшего сравнения с python и было-стало сюда было бы неплохо добавить метод декомпрессии*/
