76526b80003dcac6b615c58fdd78ce62214da68e
Maria Lipina <m.o.lip714@gmail.com>
2022-07-12 22:04:42 +0300

    задача на расстановку ферзей на шахматной доске

--- Main.java
+++ Main.java
@@ -2,7 +2,8 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        HanoiTower.doTowers(3, 'A', 'B', 'C');
+        int[][] board = new int[27][27];
+        Queens.positions(board);
         
     }
 }
