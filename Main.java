7ba9876ce4574124d0aa5923d634a5c7b03d600a
Maria Lipina <m.o.lip714@gmail.com>
2022-07-12 11:02:58 +0300

    загрузка данных из файла в коллекцию и обратно

--- Main.java
+++ Main.java
@@ -1,10 +1,6 @@
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-    
-        System.out.println(new NumsPermutation().withoutRepeat(5, 4));
 
     }
 }
