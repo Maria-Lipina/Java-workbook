becfce1987f4b1e86dc35c60e3edf9d73b059563
Maria Lipina <m.o.lip714@gmail.com>
2022-07-13 21:29:24 +0300

    Задача на то, чтобы обойти конем шахматную доску

--- Main.java
+++ Main.java
@@ -2,8 +2,7 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        int[][] board = new int[27][27];
-        Queens.positions(board);
+        Knights.moveThem(6);
         
     }
 }
