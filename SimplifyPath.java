08e461fbfc148826d5ac8963823c6f44345983df
Maria Lipina <m.o.lip714@gmail.com>
2022-07-21 19:20:42 +0300

    Simplify path - баги поправлены, но комментарии пока на месте

--- SimplifyPath.java
+++ SimplifyPath.java
@@ -6,37 +6,37 @@ public class SimplifyPath {
 
     public static void main(String[] args) {
         String path = "/home/foo/.././/java/";
-        Deque<StringBuilder> dir = new ArrayDeque<>();
-        StringBuilder res = new StringBuilder();
+        Deque<String> dir = new ArrayDeque<>();
+        StringBuilder s = new StringBuilder();
         for (int i = 1; i < path.length(); i++) {
             switch (path.charAt(i)) {
                 case '/':
                     if (path.charAt(i - 1) != '/' & path.charAt(i - 1) != '.') {
-                        res.append(path.charAt(i));
-                        dir.push(res);
-                        res.delete(0, res.length()-1);
+                        s.append(path.charAt(i));
+                        System.out.println(s);
+                        dir.push(s.toString());
+                        s.delete(0, s.length());
+                        System.out.println("s empty?" + s);
                     }
                     break;
                 case '.':
                     if (path.charAt(i - 1) == '.') {
-                        dir.pop();
+                        System.out.println(dir.toString());
+                        dir.removeFirst();
                     }
                     break;
                 default:
-                    res.append(path.charAt(i));
+                    System.out.println(path.charAt(i));
+                    s.append(path.charAt(i));
                     break;
             }
         }
-        for (StringBuilder stringBuilder : dir) {
-            res.append(dir.getFirst());
+        System.out.println("s finally empty?" + s);
+        s.append('/');
+        while (dir.peek() != null) {
+            s.append(dir.removeLast());
         }
-        System.out.println(res);
+        s.deleteCharAt(s.length()-1);
+        System.out.println(s);
     }
-
-    // до ближайшего слэша - стрингбилдер
-    // один слэш - в стрингбилдер и декью, если стрингбилдер не пуст и не содержит
-    // только другой слэш
-    // точка - предыдущий сивол - точка? Да - пропускаю. Нет - убираю из дэкью
-    // последний элемент (имя+/)
-    // в конце добавляю в стрингбилдер элементы очереди, начиная с первого
 }
