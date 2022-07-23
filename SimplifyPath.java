3741953e15a874bd8844658f36134a0def6a51c7
Maria Lipina <m.o.lip714@gmail.com>
2022-07-23 07:08:33 +0300

    Simplify path - решение найдено?

--- SimplifyPath.java
+++ SimplifyPath.java
@@ -1,14 +1,57 @@
+import java.util.ArrayDeque;
 import java.util.Arrays;
+import java.util.Deque;
 import java.util.List;
 
 public class SimplifyPath {
 
     public static void main(String[] args) {
         String input = "/home/foo/.././/java/";
-        String[] path = input.split("/"); //null home foo .. . null java (7) Короче надо перевернуть тут в LinkedList или другую коллекцию и убрать из нее лишнее, а потом склеить в стрингбилдере
-        List <String> l = Arrays.asList(path);
+        String [] path = input.split("/");  //null home foo .. . null java (7)
+        Deque <String> canonical = new ArrayDeque<>();
+        for (int i = 0; i < path.length; i++) {
 
-        // for (String str : path) {
+            switch (path[i]) {
+                case ".": System.out.println(path[i]+ " equals");
+                    
+                    break;
+                case "": System.out.println(path[i]+ "equals empty");
+                
+                break;
+
+                case "..": System.out.println(path[i]+ " nsr");
+                break;
+            
+                default: System.out.println(path[i] + " normal");
+                    break;
+            }
+        }
+        //     if (path[0].equals(path[i]) | ".".equals(path[i])) {
+        //         // canonical.offer(path[i]);
+        //         // System.out.println(canonical + " добавлен " + path[i]);
+        //         System.out.println(path[i]+ " equals");
+        //     } else {
+        //         if ("..".equals(path[i])) {
+        //             // canonical.pop();
+        //             // System.out.println(canonical + " убран " + path[i]);
+        //             System.out.println(path[i]+ " nsr");
+        //         }
+        //         System.out.println(path[i] + "not equals not nsr");
+        //     }
+        // }
+
+        // StringBuilder sb = new StringBuilder();
+        // for (int i = 0; i < path.length; i++) {
+        //     if (path[i] != "..") {
+        //         sb.append("/" + path[i]);
+        //     }
+        //     if (path[i] == "..") {
+        //         sb.delete(sb.lastIndexOf("/"), sb.length());
+        //     }
+        // }
+    }
+
+            // for (String str : path) {
         //     if (str != "") {
         //         System.out.print(str + " ");
         //     }
@@ -17,18 +60,7 @@ public class SimplifyPath {
         //     }
         // }
         // System.out.println(path.length);
-
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < path.length; i++) {
-            if (path[i] != "..") {
-                sb.append("/" + path[i]);
-            }
-            if (path[i] == "..") {
-                sb.delete(sb.lastIndexOf("/"), sb.length());
-            }
-        }
-    }
-
+    
     // Deque<String> dir = new ArrayDeque<>();
     // StringBuilder s = new StringBuilder();
     // for (int i = 1; i < path.length(); i++) {
