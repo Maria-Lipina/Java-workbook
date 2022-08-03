004fc7772d149080f24337f96e591041d4946643
Maria Lipina <m.o.lip714@gmail.com>
2022-08-03 22:18:16 +0300

    MergeStrings - небольшая задачка на слияние строк посимвольно

--- Main.java
+++ Main.java
@@ -2,12 +2,6 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {     
-    MinStack minStack = new MinStack();
-    minStack.push(-2);
-    minStack.push(0);
-    minStack.push(-3);
-    System.out.println("Min " + minStack.getMin()); //-3
-    System.out.println("Min " + minStack.getMin()); //-2 правильно
 
 }
 
