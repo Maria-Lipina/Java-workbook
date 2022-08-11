34d8cd2f709972a9fd5ac6a78d308129b4ab0d14
Maria Lipina <m.o.lip714@gmail.com>
2022-08-11 22:18:52 +0300

    Весь Wardrobe and Person - разнесла методы из Container по персонажам, не запускала

--- WardrobeAndPerson/Husband.java
+++ WardrobeAndPerson/Husband.java
@@ -1,19 +1,49 @@
 package WardrobeAndPerson;
 
-import WardrobeAndPerson.Container;
+import java.util.ArrayList;
+import java.util.Random;
+
+import WardrobeAndPerson.Container.Door;
 
 public class Husband {
 
-    public Husband() {
+    Random r = new Random();
 
-        
+    public Husband() {
 
     }
 
-        //метод take
+    private void confirm(Wife w, Container c) {
+        if (w.permission() == false) {
+            return;
+        } else {
+            c.setState();
+        }
+    }
 
-        //метод observe
+    public void observe(Wife w, Container c) {
+        confirm(w, c);
+        if (c.getState() == Door.Open)
+            System.out.println(c.items.toString());
+        else
+            System.out.println("В другой раз");
+        c.state = Door.Close;
+    }
 
-        //метод requestAllow
-    
+    public void takeItems(ArrayList<String> items, Container c, Wife w) {
+        confirm(w, c);
+        if (c.getState() == Door.Open) {
+            c.items.removeAll(items);
+            c.state = Door.Close;
+        }
+    }
+
+    public void takeItems(String item, Container c, Wife w) {
+        confirm(w, c);
+        if (c.getState() == Door.Open) {
+            c.items.remove(item);
+            c.state = Door.Close;
+        }
+    }
+
 }
