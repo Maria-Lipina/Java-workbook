fad8ae7176998340e033e5e3b488832bcb468d2a
Maria Lipina <m.o.lip714@gmail.com>
2022-08-16 21:17:04 +0300

    IsTriangle - добавлена старая задачка ещё из C# (оказывается, все так просто)

--- Main.java
+++ Main.java
@@ -1,7 +1,9 @@
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException {     
+    public static void main(String[] args) throws IOException {
+
+        System.out.println(IsTriangle.define(3, 7, 7));
 
 }
 
