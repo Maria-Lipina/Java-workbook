1f2e254d34e325e800c6ab348827c87863a7ac08
Maria Lipina <m.o.lip714@gmail.com>
2023-01-11 13:40:27 +0300

    добавила распечатку бинарного дерева в консоли

--- example/Main.java
+++ example/Main.java
@@ -1,8 +1,17 @@
 package example;
 
+import example.OneClassTasks.TreeNode;
+
 public class Main {
     public static void main(String[] args) {
+        //
+        TreeNode tree = new TreeNode(10);
+        tree.setLeft(new TreeNode(5,
+                new TreeNode(3), new TreeNode(2)));
+        tree.setRight(new TreeNode(11, new TreeNode(12)
+                ));
+        System.out.println(tree);
 
-        System.out.println("Hello world!");
+
     }
 }
