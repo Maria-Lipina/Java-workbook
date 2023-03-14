f203f974a63cf0c46719d4ce394e883d13dff482
Maria Lipina <m.o.lip714@gmail.com>
2023-03-14 22:50:30 +0300

    хэш-таблица как структура данных, реализация с занятия

---
+++ example/MyHashMap.java
@@ -0,0 +1,53 @@
+package example;
+
+public class MyHashMap {
+    Node root;
+    private class Node{
+        int value;
+        Node left;
+        Node right;
+    }
+
+    public void add(int value){
+        Node node = new Node();
+        node.value = value;
+
+        if(root == null){
+            root = node;
+        }else{
+            Node cur = root;
+            while(cur != null){
+                if(value < cur.value) {
+                    if(cur.left == null){
+                        cur.left = node;
+                        break;
+                    }
+                    cur = cur.left;
+                }
+                else {
+                    if(cur.right == null){
+                        cur.right = node;
+                        break;
+                    }
+                    cur = cur.right;
+                }
+            }
+        }
+    }
+    public boolean find(int value){
+        if(root != null){
+            Node cur = root;
+            while(cur != null){
+                if(cur.value == value)
+                    return true;
+                if(value < cur.value) {
+                    cur = cur.left;
+                }
+                else {
+                    cur = cur.right;
+                }
+            }
+        }
+        return false;
+    }
+}
